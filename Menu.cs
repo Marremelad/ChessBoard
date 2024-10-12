10c44055d52f0b53a516929ec269e0518e467ed6
Mauricio Corte <Corte.Mauricio98@gmail.com>
2024-10-12 15:59:30 +0200

    Implemented method to choose board generation options

--- Menu.cs
+++ Menu.cs
@@ -10,17 +10,47 @@ public class Menu
             new SelectionPrompt<string>()
                 .Title("Welcome to the Chess Board Generator!")
                 .PageSize(10)
-                .MoreChoicesText("[grey](Move up and down to reveal more fruits)[/]")
                 .AddChoices("Generate board", "Exit"));
 
         switch (choice)
         {
             case "Generate board":
-                Board.GenerateChessBoard(Board.GetBoardSize(), Board.GetCustomTiles(), Board.GetCustomPiece(), Board.GetPiecePlacement());
+                GenerationOptionsMenu();
                 break;
             case "Exit":
                 Console.WriteLine("Thank you for using the Chess Board Generator!");
                 break;
         }
+    }
+
+    private static void GenerationOptionsMenu()
+    {
+        int boardSize = 8;
+        var tiles = ("◼", "◻");
+        string piece = "\u2656";
+        string placement = "A1";
+        
+        var choice = AnsiConsole.Prompt(
+            new SelectionPrompt<string>()
+                .Title("Placeholder")
+                .PageSize(10)
+                .AddChoices("Board Size", "Custom Tiles", "Custom Piece", "Generate Standard Board"));
+
+        switch (choice)
+        {
+            case "Board Size":
+                boardSize = Board.GetBoardSize();
+                break;
+            case "Custom Tiles":
+                tiles = Board.GetCustomTiles();
+                break;
+            case "Custom Piece":
+                piece = Board.GetCustomPiece();
+                break;
+            case "Generate Standard Board":
+                break;
+        }
+        Board.GenerateChessBoard(boardSize, tiles, piece, placement);
+
     }
 }
