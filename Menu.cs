75975e7506a33347fb9088f101dce3c4dff0f96f
Mauricio Corte <Corte.Mauricio98@gmail.com>
2024-10-12 14:35:01 +0200

    Added Spectre.Console and created MainMenu method in Menu class

--- Menu.cs
+++ Menu.cs
@@ -1,6 +1,26 @@
-﻿namespace ChessBoard;
+﻿using Spectre.Console;
+
+namespace ChessBoard;
 
 public class Menu
 {
-    
+    public static void MainMenu()
+    {
+        var choice = AnsiConsole.Prompt(
+            new SelectionPrompt<string>()
+                .Title("Welcome to the Chess Board Generator!")
+                .PageSize(10)
+                .MoreChoicesText("[grey](Move up and down to reveal more fruits)[/]")
+                .AddChoices("Generate board", "Exit"));
+
+        switch (choice)
+        {
+            case "Generate board":
+                Board.GenerateChessBoard(Board.GetBoardSize(), Board.GetCustomTiles(), Board.GetCustomPiece(), Board.GetPiecePlacement());
+                break;
+            case "Exit":
+                Console.WriteLine("Thank you for using the Chess Board Generator!");
+                break;
+        }
+    }
 }
